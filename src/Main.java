f1f4ec1d9731b696177bef9aaebc4fece4c2f94e
Heriberto Fernandez <hfernandezaguirre@gmail.com>
2022-07-20 23:39:50 -0500

    comenzando tarea

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+Persona persona = new Persona();
+persona.getNombre("Heriberto");
+        System.out.println(persona.getEdad());
+
+}
+}
+
+
