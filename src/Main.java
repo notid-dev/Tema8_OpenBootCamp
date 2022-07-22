7deae482690a642b715e57c3f64b7434b339266f
Heriberto Fernandez <hfernandezaguirre@gmail.com>
2022-07-21 19:47:59 -0500

    Crear clase persona crear main terminar la tarea

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-Persona persona = new Persona();
-persona.getNombre("Heriberto");
+        Persona persona = new Persona();
+        persona.setNombre("Heriberto");
+        persona.setEdad(28);
+        persona.setTelefono("62245804");
+        System.out.println(persona.getNombre());
         System.out.println(persona.getEdad());
+        System.out.println(persona.getTelefono());
 
-}
+    }
 }
 
 
--- src/Persona.java
+++ src/Persona.java
@@ -7,22 +7,26 @@ public class Persona {
         this.edad = edad;
 
     }
+
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
+
     public void setTelefono(String telefono) {
         this.telefono = telefono;
     }
+
     public int getEdad() {
         return edad;
     }
+
     public String getNombre() {
         return nombre;
     }
+
     public String getTelefono() {
         return telefono;
     }
-
 
 
 }
